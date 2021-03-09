97d183894fa5081348e438bf2b4a869c7c691636
geniusidea_coworking <kukuhpbd09@gmail.com>
2021-03-09 15:40:45 +0700

    commit function

---
+++ testLibrary/src/main/java/com/saspossible/testlibrary/testClass.java
@@ -0,0 +1,9 @@
+package com.saspossible.testlibrary;
+
+import android.util.Log;
+
+public class testClass {
+    public void check1(){
+        Log.d("testClass", "check1: you are acesss");
+    }
+}
