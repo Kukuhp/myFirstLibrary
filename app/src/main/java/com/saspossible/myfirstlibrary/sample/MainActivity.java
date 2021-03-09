2937001ab5909d93e7c14d2390069acb27034433
geniusidea_coworking <kukuhpbd09@gmail.com>
2021-03-09 12:29:38 +0700

    first commit

---
+++ app/src/main/java/com/saspossible/myfirstlibrary/sample/MainActivity.java
@@ -0,0 +1,16 @@
+package com.saspossible.myfirstlibrary.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.saspossible.myfirstlibrary.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
